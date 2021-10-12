321d83b934a3ddc4c6605ba8ef88dfe8f8ce4902
Thiago <tms1_tms2@hotmail.com>
2021-10-11 23:14:31 -0300

    Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,36 @@
+import { Box, Flex, Icon, IconButton, Image, } from "@chakra-ui/react";
+import { FiChevronLeft } from "react-icons/fi"
+
+export function Header() {
+
+  return (
+    <Flex
+      as="header"
+      w="100%"
+      h={["3.125rem", "5rem", "6.25rem"]}
+      px={["1rem", "1.5rem"]}
+      align="center"
+      justify="space-between"
+    >
+      <Box w="2.5rem" minWidth="fit-content">
+        <IconButton
+          aria-label="Go back"
+          icon={<Icon as={FiChevronLeft} />}
+          fontSize={24}
+          variant="unstyled"
+          mr="0.5rem"
+        />
+      </Box>
+      <Box>
+        <Image
+          w={["60%", "100%"]}
+          height="auto"
+          src="/images/logo.svg"
+          alt="logo"
+          mx="auto"
+        />
+      </Box>
+      <Box w="2.5rem"></Box>
+    </Flex>
+  );
+}
