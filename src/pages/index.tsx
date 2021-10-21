6d80afa94494e133a6ba728ada18acd6267186be
Thiago <tms1_tms2@hotmail.com>
2021-10-21 19:02:44 -0300

    Home header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Box, Flex, Heading } from "@chakra-ui/react";
 import { Banner } from "../components/Banner";
 import { Categories } from "../components/Categories";
 
@@ -6,6 +7,23 @@ export default function Home() {
     <>
       <Banner />
       <Categories />
+      <Box
+        w={["4rem", "6rem"]}
+        my={["2.5rem", "5rem"]}
+        mx="auto"
+        h="2px"
+        bg="gray.700"
+      />
+
+      <Heading
+        mb={["1.25rem", "3.5rem"]}
+        textAlign="center"
+        fontWeight="500"
+        fontSize={["lg", "3xl", "4xl"]}
+      >
+        Vamos nessa? <br />
+        Então escolha o seu continente
+      </Heading>
     </>
   )
 }
